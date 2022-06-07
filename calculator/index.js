6bebdc96a09e062f69bdf0005ed4bc4acac7f68c
zlatamast <zlatamast@gmail.com>
2022-06-08 00:22:32 +0300

    add 3

--- calculator/index.js
+++ calculator/index.js
@@ -1,11 +1,36 @@
-function selectModels(sender) {
+function calcCost(){
 
+    let sum = 0;
+
+    let arr = [];
+    arr[0] = document.querySelector('#models').value;
+
+    let complectation = document.querySelectorAll('[name="gridRadios"]');
+    for (let i = 0; i < complectation.length; i++) {
+    if (complectation[i].checked==true) {
+        arr[1] = complectation[i].value;
+    }
 }
 
-function calcCost(){
-    if (condition) {
-        
+let extraOptions = document.querySelectorAll('.extraoption');
+for (let i = 0; i < extraOptions.length; i++) {
+    if (extraOptions[i].checked==true) {
+        arr.push(extraOptions[i].value)
     }
+}
+
+for (let i = 0; i < arr.length; i++) {
+    sum += +arr[i];
+}
+
+document.querySelector('.result').innerHTML = `
+Стоимость вашего автомобиля равна ${sum} долларов.`
+document.querySelector('.error').innerHTML = "";
+
+if (document.querySelector('#models').value=="none") {
+    document.querySelector('.error').innerHTML = "Выберите модель автомобиля";
+    document.querySelector('.result').innerHTML = "";
+} 
 }
 
 document.querySelector('#btn-result').addEventListener('click', calcCost);
